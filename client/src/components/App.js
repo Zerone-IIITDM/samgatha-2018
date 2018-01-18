ff98837f3bdc5d03d904d48d6213b5b725ec4744
Nipun Bharti <ced16i026@iiitdm.ac.in>
2018-01-18 20:19:18 +0530

    Added: Homepage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Home from './Homepage/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Header/>
+        <Home />
       </div>
     );
   }
---
+++ client/src/components/Homepage/Home.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component{
+	render(){
+		return(
+			<div className="mainContainer">
+				<div className="headContainer">
+					<div className="titleText">
+						Welcome To<br />A <span style={{color:'#56E39F'}}>Magical</span> World!
+					</div>
+				</div>
+				<div className="aboutUs">
+					<div className="leftSide">
+						<div className="leftSideText">
+							ILLUSTRATION<br />TO BE<br />PLACE HERE
+						</div>
+					</div>
+					<div className="rightSide">
+						<div className="rightSideHeaderText">
+							About Us
+						</div>
+						<div className="rightSideContainerText">
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets.
+						</div>
+						<div className="stats">
+							<div className="circle1">
+								<div className="circleStat">
+								</div>
+								<div className="statName">
+									20+ Events
+								</div>
+							</div>
+							<div className="circle1">
+								<div className="circleStat">
+								</div>
+								<div className="statName">
+									20+ Events
+								</div>
+							</div>
+							<div className="circle1">
+								<div className="circleStat">
+								</div>
+								<div className="statName">
+									20+ Events
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="joinUs">
+					<div className="joinText">
+						Come join us on this<br />fantastical journey...
+					</div>
+					<div className="registerBtn">
+						REGISTER
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Home;
