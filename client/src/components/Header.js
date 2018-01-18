63b2c0b3fe9d24a649ecf08a0255c9130314ffa9
Pranay Ankit <pranayankit414@gmail.com>
2018-01-18 18:45:37 +0530

    Added: contacts in Header component
    commit adds contacts in header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -32,9 +32,10 @@ class Header extends Component {
               <li><a href="#">About Us</a></li>
               <li><a href="/events">Events</a></li>
               <li><a href="#">Workshops</a></li>
+              <li><a href="#">Social Cause</a></li>
               <li id="heart-trigger" className="heart heart-trigger"><span className="fa fa-heart"></span></li>
               <li><a href="#">Sponsors</a></li>
-              <li><a href="#">Social Cause</a></li>
+              <li><a href="#">Contacts</a></li>
               <li><a href="/auth/google">Login/Register</a></li>
             </ul>
           </nav>
