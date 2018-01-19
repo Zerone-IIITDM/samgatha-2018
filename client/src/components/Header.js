ddf9508eb0a7ecbd18ca4e10e6c8ba45589349fa
Pranay Ankit <pranayankit414@gmail.com>
2018-01-19 18:44:16 +0530

    Modified: Header.js
    commit moves the position of heart icon to left of the Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,10 +8,10 @@ class Header extends Component {
         <header>
           <nav role='navigation'>
             <ul>
+              <li id="heart-trigger" className="heart heart-trigger"><span className="fa fa-heart"></span></li>
               <li><a href="/events">Events</a></li>
               <li><a href="#">Workshops</a></li>
               <li><a href="#">Social Cause</a></li>
-              <li id="heart-trigger" className="heart heart-trigger"><span className="fa fa-heart"></span></li>
               <li><a href="#">Sponsors</a></li>
               <li><a href="#">Contacts</a></li>
               <li><a href="/auth/google">Login/Register</a></li>
