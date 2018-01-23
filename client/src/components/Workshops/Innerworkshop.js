24d99fb6f278bf57aae60d9e3bfa3abbddf671aa
Sankar <mavsankar@gmail.com>
2018-01-23 22:49:41 +0530

    Changed:the inner workshop nav

--- client/src/components/Workshops/Innerworkshop.js
+++ client/src/components/Workshops/Innerworkshop.js
@@ -1,63 +1,88 @@
 import React,{ Component } from 'react';
 import './Innerworkshop.css';
 import '../font-awesome-4.7.0/css/font-awesome.min.css';
-
-class Card extends Component{
-	render(){
-		return(
-			<li ><a href = {this.props.link}><div className='t1' id = 'w1' style={{backgroundImage: 'url('+require(''+this.props.img1+'')+')'}}></div></a></li>	
-		);		
-	}
-}
-
 class Innerworkshop extends Component{
 
-	render(){
-		
-		
+	
+	common()
+	{
+
+		return(
+
+		<div className='outer'>  
+<div class="wrapper">
+  <div class="content">
+    <div id="mainMenu">
+      <ul>
+        <li>
+          <div class="item highlighted">
+            <a href="#" >About</a>
+          </div>
+        </li>
+        
+
+        <li>
+          <div class="item">
+            <a href="#">Venue and Timings</a>
+          </div>
+        </li>
+        <li>
+          <div class="item">
+            <a href="#">Register</a>
+          </div>
+        </li>
+      </ul>
+      
+      <div id="menuContent">
+     	<br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dui eros, 
+			vehicula eu pellentesque id, dapibus non lacus. <br/>Quisque commodo sed dolor posuere consectetur. 
+			Duis dignissim tempor erat, maximus elementum sapien mattis eget.<br/> Aliquam lacus erat,
+			 suscipit sit amet lacus ac, pellentesque 
+			 convallis nunc.<br/> Integer pellentesque libero vitae nisi placerat, sed placerat enim finibus.
+			 Proin et lorem eros. <br/>Vestibulum sed magna ullamcorper, semper velit in, pretium tellus. Proin vel bibendum 
+			 orci. 
+      </div>
+      
+    </div>
+  </div>
+  </div>
+  </div>
+		);
+	}
+	
+render(){
+	
 		if (this.props.num === "1")
 		return(
-	<div className='outer'>
-<div class="nav">
-  <ul class="parent">
-  <li><a href="#" id='about'>About <span class="plus">+</span></a>
-      <ul class="child">
-          <li><a href="#" id='workshop'>Workshop</a></li>
-        <li><a href="#">Speaker</a></li>
-      </ul>    
-    </li>
-    <li><a href="#">Prerequisites</a></li>
-    
-    <li><a href="#">Registration <span class="plus">+</span></a>
-      <ul class="child">
-          <li><a href="#">Details</a></li>
-        <li><a href="#">Register</a></li>
-      </ul>
-    </li>
-    <li><a href="#">Contact</a></li>
-  </ul>
-</div>
-	</div>
+			<div>
+			<li>{this.common()}</li>
+
+			</div>
 		);
 		else if (this.props.num === "2")
 		return (
-			<p>Sample text 2</p>
+			<li>{this.common()}</li>
+		
 		);	
 		else if (this.props.num === "3")
 		return(
-			<p>Sample text 3</p>
+			<li>{this.common()}</li>
+			
 		);
 		else if (this.props.num === "4")
 		return(
-			<p>Sample text 4</p>
+			<li>{this.common()}</li>
+			
 		);
 		else if (this.props.num === "5")
 		return(
-			<p>Sample text 5</p>
+			<li>{this.common()}</li>
+			
 		);	
 		else if (this.props.num === "6")
 		return(
-			<p>Sample text 6</p>
+			<li>{this.common()}</li>
+			
 		);
 		else
 		return(
@@ -65,6 +90,9 @@ class Innerworkshop extends Component{
 		);	
 
 	}
+	
+		
+	
 }
 
 
