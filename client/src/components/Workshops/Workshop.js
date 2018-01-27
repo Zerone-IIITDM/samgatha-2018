2f0d6b13dac9edb9c9af59c8d040d33ca1e31bbe
Nipun Bharti <ced16i026@iiitdm.ac.in>
2018-01-27 23:17:09 +0530

    Modified: Workshops template and Events

--- client/src/components/Workshops/Workshop.js
+++ client/src/components/Workshops/Workshop.js
@@ -2,54 +2,45 @@ import React,{ Component } from 'react';
 import './Workshop.css';
 import '../font-awesome-4.7.0/css/font-awesome.min.css';
 
-//Class for individual card
-class Card extends Component{
-	render(){
-		return(
-			<li><a href="#"><div className='t1' id = 'w1' style={{backgroundImage: 'url('+require(''+this.props.img1+'')+')'}}></div></a></li>	
-		);		
-	}
-}
-
 class Workshop extends Component{
-
 	render(){
-
 		return(
-			
-<div className='outer' >
-			<div className='titiletext'>
-			<b>Workshops</b><br/><br/>	
+			<div className="workshopContainer">
+				<div className="workshopHeader">
+					Workshops
+				</div>
+				<div className="workshopContent">
+	              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard<br /> dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+	            </div>
+	            <div className="workshopList">
+	            	<div className="workshopOne">
+		            	<div className="workshopImage">
+		            	</div>
+		            	<div className="workshopDetails">
+		            		<div className="workshopName">
+		            			AWS Deep Learning Workshop
+		            		</div>
+		            		<div className="speakerAndShit">
+		            			By Speaker<br/>Speaker Qualifications<br />Date<br />Venue
+		            		</div>
+		            	</div>
+		            </div>
+		            <div className="workshopOne">
+		            	<div className="workshopImage">
+		            	</div>
+		            	<div className="workshopDetails">
+		            		<div className="workshopName">
+		            			AWS Deep Learning Workshop
+		            		</div>
+		            		<div className="speakerAndShit">
+		            			By Speaker<br/>Speaker Qualifications<br />Date<br />Venue
+		            		</div>
+		            	</div>
+		            </div>
+	            </div>
 			</div>
-			<div className='text'>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dui eros, 
-			vehicula eu pellentesque id, dapibus non lacus. <br/>Quisque commodo sed dolor posuere consectetur. 
-			Duis dignissim tempor erat, maximus elementum sapien mattis eget.<br/> Aliquam lacus erat,
-			 suscipit sit amet lacus ac, pellentesque 
-			 convallis nunc.<br/> Integer pellentesque libero vitae nisi placerat, sed placerat enim finibus.
-			 Proin et lorem eros. <br/>Vestibulum sed magna ullamcorper, semper velit in, pretium tellus. Proin vel bibendum 
-			 orci. 
-			</div>
-			<div className='workshop'>
-			
-			<li className="cards"><Card img1 = './img.jpg' /></li>
-			<li className="cards"><Card img1 = './img.jpg' /></li>
-			<li className="cards"><Card img1 = './img.jpg' /></li>
-			
-			</div>
-			
-			<div className='workshop'>
-			<li className="cards"><Card img1 = './img.jpg' /></li>
-			<li className="cards"><Card img1 = './img.jpg' /></li>
-			<li className="cards"><Card img1 = './img.jpg' /></li>
-
-			</div>
-			
-</div>
-
-			);
+		);
 	}
 }
-
 
 export default Workshop;
