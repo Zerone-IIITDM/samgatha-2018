40bae969983a2085901a4bc6fb02372613b61333
akhiljasrotia <abujasrotia@gmail.com>
2018-01-28 13:52:43 +0530

    Modified: Footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 import '../font-awesome-4.7.0/css/font-awesome.min.css';
 
@@ -8,10 +9,10 @@ class Footer extends Component {
 			<footer>
 				<nav role='navigation' className="footerNav">
 					<ul className="ul1">
-						<li><i className="fa fa-instagram fa-2x" style={{ color: "white" }}> </i></li>
-						<li><i className="fa fa-facebook fa-2x" style={{ color: "white" }}> </i></li>
-						<li><i className="fa fa-twitter fa-2x" style={{ color: "white" }}> </i></li>
-						<li><i className="fa fa-youtube fa-2x " style={{ color: "white" }}> </i></li>
+						<li className="insta"><i className="fa fa-instagram fa-2x" style={{ marginTop:'8px',color: "white" }}></i></li>
+						<li className="insta"><i className="fa fa-facebook fa-2x" style={{ marginTop:'8px',color: "white" }}> </i></li>
+						<li className="insta"><i className="fa fa-twitter fa-2x" style={{ marginTop:'8px',color: "white" }}> </i></li>
+						<li className="insta"><i className="fa fa-youtube fa-2x " style={{ marginTop:'8px',color: "white" }}> </i></li>
 					</ul>
 				</nav>
 				<nav role='navigation' className="footerNav">
@@ -19,20 +20,19 @@ class Footer extends Component {
 						<li>
 							<div className="text1">
 								FAQs
-					</div>
+							</div>
 						</li>
 						<li>
 							<div className="text1">
 								Brochure
-					</div>
+							</div>
 						</li>
 						<li>
 							<div className="text1">
 								<i className="fa fa-copyright" style={{ color: "white" }}> </i>
 								WebOps,Samgatha
-					</div>
+							</div>
 						</li>
-						<li></li>
 					</ul>
 				</nav>
 			</footer>
