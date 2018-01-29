dc04c459cacdf9a78043a43a67e980ae0748946d
Nipun Bharti <ced16i026@iiitdm.ac.in>
2018-01-29 22:26:09 +0530

    Modified: Color scheme and font scheme of the website

--- client/src/components/Events/Events.js
+++ client/src/components/Events/Events.js
@@ -7,7 +7,7 @@ class Events extends Component {
       return (
           <div className="eventsContainer">
             <div className="eventsHeader">
-              EVENTS
+              Events
             </div>
             <div className="eventsContent">
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
