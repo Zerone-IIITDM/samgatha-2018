42633becf8f14a34ec6a391a717b3efe5aba2d85
Nipun Bharti <ced16i026@iiitdm.ac.in>
2018-01-31 03:39:19 +0530

    Modified: Under construction label

--- client/src/components/Events/Events.js
+++ client/src/components/Events/Events.js
@@ -10,7 +10,7 @@ class Events extends Component {
               Events
             </div>
             <div className="eventsContent">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+             
             </div>
             <div className="techAndCult">
               <Link to='/techeventslist' className="tech" style={styles.links}>
