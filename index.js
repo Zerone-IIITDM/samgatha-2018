6065406c3a6ee3cf91279c425aa78b67cdd89392
Pranay Ankit <pranayankit414@gmail.com>
2018-02-02 19:16:47 +0530

    Added: prod settings
    commit adds production settings

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const PORT = process.env.PORT || 5000;
 
 app.use(passport.initialize());
 
+if (process.env.NODE_ENV == 'production') {
+    // Serve the static react app files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Serve index.html file
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
 require('./routes/authRoutes')(app);
 
 app.listen(PORT, () => {
